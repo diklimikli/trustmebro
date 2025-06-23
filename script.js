c0e2668d876f3e08a517c9f2d73163867dbf4310
diklimikli <diklimikli@gmail.com>
2025-06-23 17:21:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   const link = document.createElement("a");
-  link.href = "public/jegy.pdf";
+  link.href = "jegy.pdf";
   link.download = "jegy.pdf";
   document.body.appendChild(link);
   link.click();
